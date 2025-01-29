c96f1fdc9ad359e64ad7112030c5b949f6646012
Zinakhan93 <Zinakhan93@mail.ru>
2025-01-29 21:16:36 +0300

    Сделано дз 3 задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        //    задача №2
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        // задача №3
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        // задача №4
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        // задача №5
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
-        // задача № 6
-        var fighter1 = 78.2;
-        var fighter2 = 87.2;
-        var totalWeight = fighter1 + fighter2;
-        System.out.println("Общая масса двух бойцов " + totalWeight + "кг.");
-        var weightDifference = fighter2 - fighter1;
-        System.out.println("Разница между массами бойцов " + weightDifference + "кг.");
-        // задача №7
-        var divisionRemainder = fighter2 % fighter1;
-        System.out.println(divisionRemainder);
-        // задача №8
-        var totalHours = 640;
-        var totalEmployees = totalHours / 8;
-        System.out.println("Всего работников в компании " + totalEmployees + " человек.");
-        var increaseEmployees = totalEmployees + 94;
-        var divisionHours = totalHours / increaseEmployees;
-        System.out.println("Если в компании работает " + increaseEmployees + " человек, то всего " + divisionHours + " часов работы может быть поделено между сотрудниками.");
+        // задача №1
+        int a = 1000000;
+        System.out.println("Значение пременной a " + " с типом int " + " равно " + a);
+        byte b = 125;
+        System.out.println("Значение пременной b " + " с типом byte " + " равно " + b);
+        short c = 30000;
+        System.out.println("Значение пременной c " + " с типом short " + " равно " + c);
+        long d = 250000000;
+        System.out.println("Значение пременной d " + " с типом long " + " равно " + d);
+        float e = 3.25F;
+        System.out.println("Значение пременной e " + " с типом float " + " равно " + e);
+        double i = 2.67;
+        System.out.println("Значение пременной i " + " с типом double " + " равно " + i);
+        // задача № 2
+        double z = 27.12;
+        long t = 987678965549L;
+        float h = 2.786F;
+        short m = 569;
+        short k = -159;
+        short l = 27897;
+        byte c = 67;
 
 
     }
