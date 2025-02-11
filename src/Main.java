caefded904d8fa41508d1368675228adc12e8862
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-11 23:32:36 +0300

    Д3-6 задача 5,6

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,21 @@ public class Main {
             System.out.println(i);
         }
         // задача 3
-        for (int i= 0; i < 17; i = i+2){
+        for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
         // задача 4
-        for ( int i = 10; i> -11; i--){
+        for (int i = 10; i > -11; i--) {
             System.out.println(i);
         }
-
+        // задача 5
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является вискокосным");
+        }
+        // задача 6
+        for (int i = 7; i < 99; i = i +7){
+            System.out.println(i);
+        }
 
     }
 }
