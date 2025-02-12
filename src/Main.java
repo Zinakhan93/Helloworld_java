9186dc53ee554fb8ec0ef208bc3ebda3f5b50cf1
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-12 23:38:31 +0300

    ДЗ 7 задача 1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import com.sun.jdi.Value;
 public class Main {
     public static void main(String[] args) {
         // задача 1
-        int salary= 15000;
+        int salary = 15000;
         int total = 0;
-        while (int i; i <=2459000 ; i++;){
-            total = total+salary;
-            System.out.println("Месяц " + i + " , сумма накоплений равна " + total);
+        int i = 0;
+        while ( total <= 2459000) {
+            total=total+salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total);
+            i++;
         }
 
-
+        //
 
 
 
