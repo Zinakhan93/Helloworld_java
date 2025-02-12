5b615daa7d308a918fddc4e8f2cad016a703e253
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-12 21:27:25 +0300

    Д3-6 изменения

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println(i);
         }
         // задача 3
-        for (int i = 0; i < 17; i = i + 2) {
+        for (int i = 0; i < 17; i += 2) {
             System.out.println(i);
         }
         // задача 4
