899ea096c9bc85de07e1a30ac9a6aeb695554464
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-14 00:03:53 +0300

    ДЗ 7 задача 2

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,22 @@ public class Main {
         int salary = 15000;
         int total = 0;
         int i = 0;
-        while ( total <= 2459000) {
-            total=total+salary;
+        while (total <= 2459000) {
+            total = total + salary;
             System.out.println("Месяц " + i + " сумма накоплений равна " + total);
             i++;
         }
-
+        //задача 2
+        int number = 0;
+        while (number < 10) {
+            number++;
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        for (int b = 10; b > 0; b--) {
+            System.out.print(b + " ");
+        }
         //
-
 
 
     }
