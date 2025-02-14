05ca9abddc2f3a6961ba8f6aab7b75eb8024be3a
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-15 00:28:19 +0300

    ДЗ 7 задача 4 и 5

--- src/Main.java
+++ src/Main.java
@@ -24,11 +24,30 @@ public class Main {
         //задача 3
         int population = 12_000_000;
         int birthRate = 17;
-        int mortality =8;
-        for (int v =0; v<=10; v++){
-            population = population +  population/1000*17  - population/1000*8;
-            System.out.println("Год " + v + " численность населения составляет " + population);
+        int mortality = 8;
+        for (int year = 0; year <= 10; year++) {
+            population = population + population / 1000 * 17 - population / 1000 * 8;
+            System.out.println("Год " + year + " численность населения составляет " + population);
         }
+        // задача 4
+        int pay = 15000;
+        int totalPay = 0;
+        for (int month = 0; totalPay <= 12_000_000; month++) {
+            totalPay = totalPay + totalPay / 100 * 7;
+            totalPay = totalPay + pay;
+            System.out.println("Месяц " + month + " " + totalPay);
+        }
+        // задача 5 (та же, что и 4 задача, только доблем условный опреатор if + к переменным добавила цифры)
+        int pay1 = 15000;
+        int totalPay2 = 0;
+        for (int month = 1; totalPay2 <= 12_000_000; month++) {
+            totalPay2 = totalPay2 + totalPay2 / 100 * 7;
+            totalPay2 = totalPay2 + pay1;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + " " + totalPay2);
+            }
+        }
+        // задача 6
 
 
     }
