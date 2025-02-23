8dc0a01ea4b3a8c05cb76bf5ac8b829e3bf8e0d3
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-23 16:10:11 +0300

    hw-9 задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,15 @@
-import com.sun.jdi.Value;
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // задача 1
-        int[] numbers = new int[]{1, 2, 3};
-        double[] fractional = {1.57, 7.654, 9.986};
-        short[] small = {4, 5, 6};
+        int []arr= new int[]{1,3,4,5,6};
+        int sum =0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println( "Сумма затрат за месяц сотавила "+ sum);
 
-        // задача 2
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        System.out.println(numbers[0] + "," + numbers[1] + "," + numbers[2]);
-        fractional[0] = 1.57;
-        fractional[1] = 7.654;
-        fractional[2] = 9.986;
-        System.out.println(fractional[0] + "," + fractional[1] + "," + fractional[2]);
-        small[0] = 4;
-        small[1] = 5;
-        small[2] = 6;
-        System.out.println(small[0] + "," + small[1] + "," + small[2]);
-        // задача 3
-        System.out.println("Задача 3");
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.print(numbers[i]);
-            if (i != 0) {
-                System.out.print(",");
-            }
-        }
-        System.out.println();
-        for (int i = fractional.length - 1; i >= 0; i--) {
-            System.out.print(fractional[i]);
-            if (i != 0) {
-                System.out.print(",");
-            }
 
-        }
-        System.out.println();
-        for (int i = small.length - 1; i >= 0; i--) {
-            System.out.print(small[i]);
-            if (i != 0) {
-                System.out.print(",");
-            }
-        }
-        System.out.println(" ");
-        System.out.println("Задача 4");
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                numbers[i] += 1;
-            }
-        }
-        System.out.print(Arrays.toString(numbers) + " ");
-        System.out.println();
-        for (int i = 0; i < fractional.length; i++) {
-            if (fractional[i] % 2 != 0) {
-                fractional[i] += 1;
-            }
-        }
-        System.out.print(Arrays.toString(fractional) + " ");
-        System.out.println();
-        for (int i = 0; i < small.length; i++) {
-            if (small[i] % 2 != 0) {
-                small[i] = +1;
-            }
-        }
-        System.out.println(Arrays.toString(small) + " ");
 
     }
 }
