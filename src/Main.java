f07611c0293d3ada4af38aaf63113a6f161b397e
Zinakhan93 <Zinakhan93@mail.ru>
2025-02-24 23:07:22 +0300

    hw-9 исправления

--- src/Main.java
+++ src/Main.java
@@ -11,24 +11,24 @@ public class Main {
         System.out.println("Сумма затрат за месяц сотавила " + sum);
         // задача 2
         int[] arr2 = {18, 25, 35, 66, 75};
-        int maxArr2 = -1;
-        int minArre2 = MAX_VALUE;
+        int MaxArr2 = -1;
+        int MinArre2 = MAX_VALUE;
         for (int i = 0; i < arr2.length; i++) {
-            if (arr2[i] > maxArr2) {
-                maxArr2 = arr2[i];
+            if (arr2[i] > MaxArr2) {
+                MaxArr2 = arr2[i];
             }
-            if (arr2[i] < minArre2) {
-                minArre2 = arr2[i];
+            if (arr2[i] < MinArre2) {
+                MinArre2 = arr2[i];
             }
         }
-        System.out.println("Макимальная сумма затрат сотавила " + maxArr2 + " рублей. Минимальная сумма затрат составила " + minArre2 + " рублей.");
+        System.out.println("Макимальная сумма затрат сотавила " + MaxArr2 + " рублей. Минимальная сумма затрат составила " + MinArre2 + " рублей.");
         // задача 3
         double[] weeks = {15.5, 45, 37.8, 25, 41.35};
         double oll = 0;
-        for (int i = 0; i < weeks.length ; i++) {
+        for (int i = 0; i < weeks.length; i++) {
             oll += weeks[i];
         }
-        oll = oll / 5;
+        oll = oll / weeks.length;
         System.out.println("Средняя сумма трат за месяц составила " + oll + " рублей");
         // Задача 4
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
