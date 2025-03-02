81e7f0e81a7ab35a5bcb337eaa2c6b9c35d70a12
Zinakhan93 <Zinakhan93@mail.ru>
2025-03-03 02:06:51 +0300

    Дз 11, задача 1 и 3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,38 @@
 import java.time.LocalDate;
 
 public class Main {
-    public  static void checkLeapYear( int year) {
+    public static void checkLeapYear(int year) {
         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            System.out.println (" Год является высокосным");
+            System.out.println(" Год является высокосным");
         } else {
-            System.out.println (" Год  не является вискосным");
+            System.out.println(" Год  не является вискосным");
         }
-     }
-     public static void downloadАpplication ( int clientOsFon, int clientDeviceYear){
-         if (clientOsFon == 0 && clientDeviceYear <= 2015) {
-             System.out.println("Установите облегченную версию приложения для IOS");
-         } else if (clientOsFon == 0) {
-             System.out.println("Установите версию приложения для IOS  по ссылке");
-         }
-         if (clientOsFon == 1 && clientDeviceYear <= 2015) {
-             System.out.println("Установите облегченную версию приложения для Android");
-         } else if (clientOsFon == 1) {
-             System.out.println("Установите версию приложения для Android по ссылке");
-         }
+    }
 
-     }
+    public static void downloadАpplication(int clientOsFon, int clientDeviceYear) {
+        if (clientOsFon == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS");
+        } else if (clientOsFon == 0) {
+            System.out.println("Установите версию приложения для IOS  по ссылке");
+        }
+        if (clientOsFon == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android");
+        } else if (clientOsFon == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1; // 1 день
+        } else if (deliveryDistance <= 60) {
+            return 2; // 2 дня
+        } else if (deliveryDistance <= 100) {
+            return 3; // 3 дня
+        } else {
+            return 0; // Доставка не осуществляется
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Задача 1");
@@ -30,10 +42,20 @@ public class Main {
         System.out.println("Задача 2");
         int clientOsFon = 1;
         int clientDeviceYear = LocalDate.now().getYear();
-        downloadАpplication(clientOsFon,clientDeviceYear);
+        downloadАpplication(clientOsFon, clientDeviceYear);
 
         System.out.println("Задача 3");
+        // Расчет дней доставки
+        int deliveryDistance = 95; // Замените на интересующее вас расстояние
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+
+        if (deliveryDays > 0) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
+
 
     }
-
 }
+
