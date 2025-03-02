dedc702e362a7646a7ef3e692917ff27d96cf572
Zinakhan93 <Zinakhan93@mail.ru>
2025-03-03 00:30:16 +0300

    Дз 11, задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 
 
 public class Main {
+    public  static void checkLeapYear( int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println (" Год является высокосным");
+        } else {
+            System.out.println (" Год  не является вискосным");
+        }
+     }
+
     public static void main(String[] args) {
-        // задача 1
-        System.out.println("Задача 1"); // задача на сложение строк
-        String firstName = "Иван";
-        String middleName = "Иванович";
-        String lastName = "Иванов";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Задача 1");
+        int year = 2021;
+        checkLeapYear(year);
 
-        System.out.println("Задача 2");// вывести ФИО заглавными, с ипользованием метода .toUpperCase()
-        System.out.println("Данные Ф.И.О. сотрдуника для заполнения отчета - " + fullName.toUpperCase());
-
-        System.out.println("Задача 3");// замена символа
-        String fullName2 = "Иванов Семён Семёнович";
-        String fullName2True = fullName2.replace("ё", "е");
-        System.out.println("Данные Ф.И.О. сотрудника - " + fullName2True);
-
-
+        System.out.println("Задача 2");
     }
 
 }
