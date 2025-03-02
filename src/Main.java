69d68fbb421ccf6893d58c5a3f2da1b626d577a2
Zinakhan93 <Zinakhan93@mail.ru>
2025-03-03 00:53:15 +0300

    Дз 11, задача 1 и 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.time.LocalDate;
 
 public class Main {
     public  static void checkLeapYear( int year) {
@@ -8,6 +8,19 @@ public class Main {
             System.out.println (" Год  не является вискосным");
         }
      }
+     public static void downloadАpplication ( int clientOsFon, int clientDeviceYear){
+         if (clientOsFon == 0 && clientDeviceYear <= 2015) {
+             System.out.println("Установите облегченную версию приложения для IOS");
+         } else if (clientOsFon == 0) {
+             System.out.println("Установите версию приложения для IOS  по ссылке");
+         }
+         if (clientOsFon == 1 && clientDeviceYear <= 2015) {
+             System.out.println("Установите облегченную версию приложения для Android");
+         } else if (clientOsFon == 1) {
+             System.out.println("Установите версию приложения для Android по ссылке");
+         }
+
+     }
 
     public static void main(String[] args) {
         System.out.println("Задача 1");
@@ -15,6 +28,12 @@ public class Main {
         checkLeapYear(year);
 
         System.out.println("Задача 2");
+        int clientOsFon = 1;
+        int clientDeviceYear = LocalDate.now().getYear();
+        downloadАpplication(clientOsFon,clientDeviceYear);
+
+        System.out.println("Задача 3");
+
     }
 
 }
