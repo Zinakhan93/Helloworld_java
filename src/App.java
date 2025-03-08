f4f1c128f79d4f2d44954874047604f49c9d2eda
Zinakhan93 <Zinakhan93@mail.ru>
2025-03-09 01:40:11 +0300

    Д3-12

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Аuthor author1 = new Аuthor("Александр", "Пушкин");
+        System.out.println(author1.getFirsname() + " " + author1.getLasname());
+    }
+
+
+}
